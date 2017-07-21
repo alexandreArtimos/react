9e7911e85dd080dfd5e5394d501037a1f88b2bc5
alexandre <alexandre.artimos@gmail.com>
2017-07-20 23:23:00 -0300

    adicionando arquivos

---
+++ src/Livro.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
+import PubSub from 'pubsub-js';
+import TratadorErros from './TratadorErros';
+
+
+
+class TabelaLivros extends Component {
+
+render() {
+  return (
+
+    <div>
+    <table className="pure-table">
+    <thead>
+    <tr>
+    <th>Titulo</th>
+    <th>Preco</th>
+    <th>Autor</th>
+    </tr>
+    </thead>
+    <tbody>
+    {
+      this.props.lista.map(function(livro) {
+        return (
+          <tr key={livro.id}>
+          <td>{livro.titulo}</td>
+          <td>{livro.preco}</td>
+          <td>{livro.autor.nome}</td>
+          </tr>
+        )
+
+      })
+    }
+    </tbody>
+    </table>
+    </div>
+
+  );
+}
+
+}
+
+class FormularioLivro extends Component {
+
+  constructor() {
+    super();
+    this.state = {titulo:'',preco:'',autorId:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setTitulo = this.setTitulo.bind(this);
+    this.setPreco = this.setPreco.bind(this);
+    this.setAutorId = this.setAutorId.bind(this);
+  }
+
+  enviaForm(evento) {
+    evento.preventDefault();
+    $.ajax({
+      url:'http://localhost:8080/api/livros',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({titulo:this.state.titulo,preco:this.state.preco,autorId:this.state.autorId}),
+      success: function(novaListagem){
+        PubSub.publish('atualiza-lista-livros',novaListagem);
+        this.setState({titulo:'',preco:'',autorId:''});
+      }.bind(this),
+      error: function(resposta){
+        if (resposta.status === 400) {
+          new TratadorErros().publicaErros(resposta.responseJSON);
+        }
+        console.log("erro");
+      },beforeSend: function () {
+        PubSub.publish("limpa-erros",{});
+
+      }
+    });
+  }
+  setTitulo(evento){
+    this.setState({titulo:evento.target.value});
+  }
+
+  setPreco(evento){
+    this.setState({preco:evento.target.value});
+  }
+
+  setAutorId(evento){
+    this.setState({autorId:evento.target.value});
+  }
+
+  render() {
+    return (
+      <div className="pure-form pure-form-aligned">
+      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+      <InputCustomizado id="titulo" type="text" name="titulo" value={this.state.titulo} onChange={this.setTitulo} label="Título"/>
+      <InputCustomizado id="Preco" type="text" name="preco" value={this.state.preco} onChange={this.setPreco} label="Preço"/>
+      <div className="pure-control-group">
+        <label htmlFor="autorId">Autor</label>
+        <select value={this.state.autorId} id="autorId" name="autorId" onChange={this.setAutorId}>
+          <option value="">Selecione autor</option>
+          {
+            this.props.autores.map(function(autor) {
+                return <option value={autor.id}>{autor.nome}</option>
+            })
+
+          }
+          </select>
+      </div>
+      <BotaoSubmitCustomizado label="Gravar"/>
+      </form>
+      </div>
+    );
+
+
+  }
+
+}
+
+export default class LivroBox extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {lista : [], autores:[]};
+
+  }
+  componentDidMount() {
+    console.log("didMount");
+    $.ajax ( {
+        url:"http://localhost:8080/api/livros",
+        dataType:'json',
+        success:function(resposta) {
+          this.setState({lista:resposta});
+        }.bind(this)
+      }
+    );
+    $.ajax ( {
+        url:"http://localhost:8080/api/autores",
+        dataType:'json',
+        success:function(resposta) {
+          this.setState({autores:resposta});
+        }.bind(this)
+      }
+    );
+
+  PubSub.subscribe('atualiza-lista-livros', function(topico, novaLista) {
+    this.setState({lista:novaLista});
+
+  }.bind(this));
+}
+
+
+
+
+  render() {
+    return (
+
+            <div>
+              <div className="header">
+              <h1>Cadastro de Livros</h1>
+              </div>
+              <div className="content" id="content">
+                <FormularioLivro autores={this.state.autores}/>
+                <TabelaLivros lista={this.state.lista}/>
+
+              </div>
+
+            </div>
+    );
+
+  }
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
+import Home from './Home';
+import './index.css';
 
-ReactDOM.render(
-  <App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+        <Router>
+            <App>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/autor" component={AutorBox}/>
+                        <Route path="/livro" component={LivroBox}/>
+                    </Switch>
+            </App>
+        </Router>
+
+), document.getElementById('root'));
